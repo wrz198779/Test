8fd1c7aba9b24214f0152918bb9ccbb4eb606033
Tony <tony@gmail.com>
2020-04-24 13:40:20 +0800

    number2

--- app/src/main/java/activitytest/example/com/coolweather/gson/AQI.java
+++ app/src/main/java/activitytest/example/com/coolweather/gson/AQI.java
@@ -1,10 +1,18 @@
 package activitytest.example.com.coolweather.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by wurenzhong on 2020-04-23.
  */
 
 public class AQI {
-    public String aqi;
-    public String pm25;
+    @SerializedName("city")
+    public AQICity city;
+    public class AQICity {
+        @SerializedName("aqi")
+        public String aqi;
+        @SerializedName("pm25")
+        public String pm25;
+    }
 }
